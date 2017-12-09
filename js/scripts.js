0170e428805685c0fca0b104c92a65777d45ed79
June Muoti <june.muoti@gmail.com>
2017-12-09 22:42:36 +0300

    add grids

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $("form#pingpong").submit(function(event) {
+      event.preventDefault();
+    var inputtedNum = $("#number").val();
+
+    var output = pingpong(inputtedNum);
+    output.forEach(function(element){
+    $("#list").append("<li>" +element );
+  });
+});
+});
+
+
+  function pingpong(inputtedNum){
+    var output=[];
+    for (i=1; i<=inputtedNum;i++){
+      if(i%15 === 0){
+      output.push("pingpong");
+    }
+      else if (i%5 === 0) {
+        output.push("pong");
+      }
+      else if (i%3 === 0) {
+        output.push("ping")
+      }
+      else {
+        output.push(i);
+      }
+    }
+
+  return output;
+}
